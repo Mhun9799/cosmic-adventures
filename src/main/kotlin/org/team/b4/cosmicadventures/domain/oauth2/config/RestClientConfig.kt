6b87020481f985db9a3c17373e39afeabfb21269
chanmo2311211 <abczza@naver.com>
2024-03-11 15:48:54 +0900

    Feat: 카카오톡 소셜로그인 구현

---
+++ src/main/kotlin/org/team/b4/cosmicadventures/domain/oauth2/config/RestClientConfig.kt
@@ -0,0 +1,16 @@
+package org.team.b4.cosmicadventures.domain.oauth2.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+
+
+@Configuration
+class RestClientConfig {
+
+    @Bean
+    fun restClient(): RestClient {
+        return RestClient.builder()
+            .build()
+    }
+}
