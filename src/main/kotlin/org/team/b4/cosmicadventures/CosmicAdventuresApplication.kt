8a25aba3043083c8b49462ae474068e60a7df1a1
hyeonseok <ghostpar12@naver.com>
2024-03-14 03:16:08 +0900

    [Refactor] 주석제거

--- src/main/kotlin/org/team/b4/cosmicadventures/CosmicAdventuresApplication.kt
+++ src/main/kotlin/org/team/b4/cosmicadventures/CosmicAdventuresApplication.kt
@@ -4,7 +4,6 @@ package org.team.b4.cosmicadventures
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 
