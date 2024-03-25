29b81a633af657fb6c8595b4cc9c591ed18bbd78
Mhun9799 <muhun1030@gmail.com>
2024-03-25 11:46:14 +0900

    Feat: Eonet 저장기능구현

--- src/main/kotlin/org/team/b4/cosmicadventures/domain/donki/repository/DonkiRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.team.b4.cosmicadventures.domain.donki.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.team.b4.cosmicadventures.domain.donki.model.News
-
-
-interface DonkiRepository : JpaRepository<News,Long> {
-}
